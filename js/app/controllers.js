c6eaec1b016c1b935b358c1c537d4bb79ca684cb
Pauline Ndiaye <aysha.pauline@gmail.com>
2016-04-19 11:40:52 +0200

    instructions

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -2,3 +2,17 @@ var myApp = angular.module('myApp',[])
   myApp.controller('controllerOne',function($scope){
 
   });
+
+//Creation d'une application de note :
+// - Savoir combien de carac restant
+// - bouton submit qui marche selon le nbr de caract
+// - recuperation du text et push dans un tableau
+// - if submitt click -> bull info = notif (ng-class)
+// - if effacer clicked = efface msg
+
+/*MessageNote : variable stockant le contenant de la note	
+info :message affiché (modifié,sauvegardé...)	
+save() :Met à jour le modèle (messageNote)	
+clear():vide messageNote	
+count() :calcule le nombre de caractères restants (sur 100)	
+*/
