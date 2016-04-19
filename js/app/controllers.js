2be502e85a356e1edeca019031b67830e659e952
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-04-19 12:04:29 +0200

    Pauline

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp',[])
   myApp.controller('controllerOne',function($scope){
-  	$scope.compteur = function(){
-  		$scope.textarea.length
+    $scope.longueurMax = 100;
+    $scope.compteur = function(){
   	};
   });
 
@@ -12,9 +12,9 @@ var myApp = angular.module('myApp',[])
 // - if submitt click -> bull info = notif (ng-class)
 // - if effacer clicked = efface msg
 
-/*MessageNote : variable stockant le contenant de la note	
-info :message affiché (modifié,sauvegardé...)	
-save() :Met à jour le modèle (messageNote)	
-clear():vide messageNote	
-count() :calcule le nombre de caractères restants (sur 100)	
+/*MessageNote : variable stockant le contenant de la note
+info :message affiché (modifié,sauvegardé...)
+save() :Met à jour le modèle (messageNote)
+clear():vide messageNote
+compteur() :calcule le nombre de caractères restants (sur 100)
 */
